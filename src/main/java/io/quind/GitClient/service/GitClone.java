8d9bb6de1c2a77090f13becdd92462d64fbf93ad
andres.salcedo1 <andres.salcedo@quind.io>
2021-04-08 14:58:29 -0500

    eliminando smell en gitclone

--- src/main/java/io/quind/GitClient/service/GitClone.java
+++ src/main/java/io/quind/GitClient/service/GitClone.java
@@ -4,11 +4,10 @@ import java.io.File;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.api.errors.InvalidRemoteException;
-import org.eclipse.jgit.api.errors.TransportException;
+
 
 public class GitClone {
-    public static void main(String[] args) throws InvalidRemoteException, TransportException, GitAPIException {
+    public static void main(String[] args) throws  GitAPIException {
         Git.cloneRepository()
                 .setURI("https://github.com/eclipse/jgit.git")
                 .setDirectory(new File("/home/andres/Documents/clonehere"))
